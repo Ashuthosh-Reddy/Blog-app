43db2e4f9edd079b4154c3a452d6eb19da8cb9ec
Ashuthosh-Reddy <125697336+Ashuthosh-Reddy@users.noreply.github.com>
2024-07-11 15:16:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from './api';
-import PostList from './components/PostList';
-import PostForm from './components/PostForm';
-import PostDetail from './components/PostDetail';
+import PostList from './components/post';
+import PostForm from './components/form';
+import PostDetail from './components/details';
 
 const App = () => {
   return (
